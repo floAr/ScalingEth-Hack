c3a1461e0155ec51c9736aef7e089cbccd84cbb0
Florian Uhde <floar@mx.de>
2021-04-25 10:51:42 +0200

    added logo and title

---
+++ frontend/src/lib/logo/title.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export const textStroke = '#220211'
+  export const textFill = '#110111'
+</script>
+
+<div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 245 40"
+    ><g fill={textFill} stroke={textStroke}>
+      <text
+        x="49%"
+        y="68%"
+        width="100%"
+        height="100%"
+        stroke-width=".264583"
+        font-size="20.5833"
+        font-weight="400"
+        letter-spacing="0"
+        word-spacing="0"
+        text-anchor="middle"
+        font-family="Cinzel Decorative"
+      >
+        <tspan>Secret Manufaktur</tspan>
+      </text>
+    </g>
+  </svg>
+</div>
