4c70e7f78f58fec6c1dceb39cdf22224d4d21154
Florian Uhde <floar@mx.de>
2021-04-25 21:59:23 +0200

    header styling

--- frontend/src/lib/layout/Header.svelte
+++ frontend/src/lib/layout/Header.svelte
@@ -4,36 +4,60 @@
 </script>
 
 <div class="nav-bar">
-  <Logo/>
   <div />
-  <nav>
-    <a href=".">home</a>
-    <a href="mint">mint</a>
-  </nav>
-  <div class="theme-icon">
-    <ThemeIcon size={25} />
-  </div>
-</div>
 
+    <a href=".">Feed</a>
+    <div class="header-logo">
+      <Logo/>
+    </div>
+    <a href="mint">Manufaktur</a>
+
+  <!-- <div class="theme-icon">
+    <ThemeIcon size={25} />
+  </div> -->
+  <div />
+</div>
+<hr class="rounded">
 <style lang="scss">
 
+.header-logo{
+  width: 100px;
+}
   .nav-bar {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 7vh;
-    width: 100vw;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    margin-top: 5px;
+    flex-direction: row;
+    font-family: 'Cinzel Decorative',  cursive;
+    font-weight: 700;
+    font-size: large;
+    width: 100vw;
+    margin-top: 5px; 
+    color: var(--theme-colors-text);
+    /* position: absolute;
+    top: 0;
+    left: 0;
+    height: 7vh;
+    display: flex;
+    */
   }
 
   .nav-bar a {
     padding: 10px;
+    margin-top:50px;
+    text-decoration: dotted;
   }
 
   .theme-icon {
+    margin-top:30px;
     padding: 0 10px;
   }
+
+  /* Rounded border */
+hr.rounded {
+  border-top: 1px solid #bbb;
+  border-radius: 5px;
+  width: 90vw;
+  transform: translateY(-5px);
+}
 </style>
