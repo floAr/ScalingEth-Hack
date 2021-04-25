3638f0a0182505ef120ccd2fa9b19c4e48e714ba
Florian Uhde <floar@mx.de>
2021-04-25 20:27:12 +0200

    add sass entry and theme colors

---
+++ frontend/src/themes.ts
@@ -0,0 +1,59 @@
+
+
+export const themes = {
+    light: {
+      colors: {
+        text: '#151A20',
+        background: {
+          _: '#f1f1f1',
+          contrast: '#b1b1b1',
+        },
+        gray: '#5F5F6B',
+        purple: '#816DA8',
+        red: '#D53A2C',
+        orange: '#EA7534',
+        sand: '#E5C7A3',
+        yellow: '#FFD72E',
+        green: '#5AA361',
+        azure: '#3EB7AC',
+        blue: '#4195C4',
+
+      },
+    },
+    dark: {
+      colors: {
+        text: '#f1f1f1',
+        background: {
+          _: '#27323a',
+          contrast: '#151A20',
+        },
+        gray: '#BCBCCC',
+        purple: '#D2BEED',
+        red: '#EF726C',
+        orange: '#FFAA88',
+        sand: '#FFE6D2',
+        yellow: '#FFEB97',
+        green: '#B1D8A3',
+        azure: '#9AE8DA',
+        blue: '#8FD2EA',
+      },
+    },
+    // high_key: {
+    //   colors: {
+    //     text: '#151A20',
+    //     background: {
+    //       _: '#FFFFFF',
+    //       contrast: '#b1b1b1',
+    //     },
+    //     gray: '#5F5F6B',
+    //     purple: '#7A5CD6',
+    //     red: '#FF144E',
+    //     orange: '#FF6510',
+    //     sand: '#FFCE99',
+    //     yellow: '#FFEC00',
+    //     green: '#25A02B',
+    //     azure: '#00DDBE',
+    //     blue: '#00B3EA',
+    //   },
+    // },
+  }
