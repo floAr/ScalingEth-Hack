40da024c66f186084496b1a9a157e412a8c70007
hidotp <38942179+hidotp@users.noreply.github.com>
2021-05-02 16:00:40 +0300

    finally centered header holy moly

--- frontend/src/lib/layout/Header.svelte
+++ frontend/src/lib/layout/Header.svelte
@@ -7,13 +7,12 @@
 
   async function connect() {
     await SecretStore.setBrowserProvider('holodeck-2')
-    
   }
 </script>
 
 <div class="nav-bar">
   <div class="nav-container">
-    <div class="header-container">
+    <div class="header-container left">
       <div class="header-link">
         <a href=".">Feed</a>
       </div>
@@ -25,15 +24,15 @@
       }}
     >
       <Logo />
-      {#if $status==='undefined'}
+      {#if $status === 'undefined'}
         <div class="toast-login">click to connect keplr</div>
-        {:else if $status === 'working'}
+      {:else if $status === 'working'}
         <div class="toast-login">working</div>
-        {:else}
+      {:else}
         <div class="toast-login">{$SecretStore.account?.address}</div>
       {/if}
     </div>
-    <div class="header-container">
+    <div class="header-container right">
       <div class="header-link">
         <a href="mint">Manufaktur</a>
       </div>
@@ -43,7 +42,20 @@
 <Seperator />
 
 <style lang="scss">
+  .nav-bar {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+    font-family: 'Cinzel Decorative', cursive;
+    font-weight: 700;
+    font-size: large;
+    width: 100vw;
+    color: var(--theme-colors-text);
+    margin: 10px 0;
+  }
   .nav-container {
+    position: relative;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -67,17 +79,18 @@
   }
 
   .header-container {
+    position: absolute;
     display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    min-width: 20vw;
-    padding-left: 30px;
+    align-items: center;
+    height: 100%;
   }
 
-  .header-container:nth-child(1) {
-    justify-content: flex-end;
-    padding-left: none;
-    padding-right: 30px;
+  .header-container.left {
+    right: 120%;
+  }
+
+  .header-container.right {
+    left: 120%;
   }
 
   .header-link {
@@ -110,18 +123,6 @@
   }
   .header-link a:visited {
     color: var(--theme-colors-text);
-  }
-  .nav-bar {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    flex-direction: row;
-    font-family: 'Cinzel Decorative', cursive;
-    font-weight: 700;
-    font-size: large;
-    width: 100vw;
-    color: var(--theme-colors-text);
-    margin: 10px 0;
   }
   .theme-icon {
     margin-top: 30px;
