bddef41603778545181fbe59e80cce04326c548e
Florian Uhde <floar@mx.de>
2021-05-07 21:44:13 +0200

    reset vk store

--- frontend/src/lib/modules/secret/viewingkey-store.ts
+++ frontend/src/lib/modules/secret/viewingkey-store.ts
@@ -5,45 +5,47 @@ import { persist, localStorage } from "@macfja/svelte-persistent-store";
 import { writable } from "svelte/store";
 import { SecretStore } from "./secret-store";
 
-export type viewingKeyPair = {
-    key: string,
-    address: string
-}
+// export type viewingKeyPair = {
+//     key: string,
+//     address: string
+// }
 
 
-export const createStore = () => {
-    const { subscribe, update } = persist<viewingKeyPair[]>(writable<viewingKeyPair[]>([]), localStorage(), 'viewingKeys')
+// export const createStore = () => {
+//     const { subscribe, update } = persist<viewingKeyPair[]>(writable<viewingKeyPair[]>([]), localStorage(), 'viewingKeys')
 
-    const addViewingKey = async (address: string) => {
-        const entropy = SecretStore.getEntropy();
-        const viewingkey = await tokenContract.SendTransaction({
-            create_viewing_key: {
-                entropy,
-                padding: SecretStore.createPadding(entropy.length, 128)
-            }
-        })
-        update(state => {
-            const filtered = state.filter(vkp => { return vkp.address !== address })
-            return [...filtered, { address, key: viewingkey.viewing_key.key }]
-        })
-        console.log(viewingkey)
-        return viewingkey.viewing_key.key
-    }
+//     const addViewingKey = async (address: string) => {
+//         const entropy = SecretStore.getEntropy();
+//         const viewingkey = await tokenContract.SendTransaction({
+//             create_viewing_key: {
+//                 entropy,
+//                 padding: SecretStore.createPadding(entropy.length, 128)
+//             }
+//         })
+//         update(state => {
+//             const filtered = state.filter(vkp => { return vkp.address !== address })
+//             return [...filtered, { address, key: viewingkey.viewing_key.key }]
+//         })
+//         console.log(viewingkey)
+//         return viewingkey.viewing_key.key
+//     }
 
-    const getViewingKey = (address: string): string | undefined => {
-        let result: undefined | string = undefined
-        subscribe(value => {
-            value.forEach(vkp => {
-                if (vkp.address === address)
-                    result = vkp.key
+//     const getViewingKey = (address: string): string | undefined => {
+//         let result: undefined | string = undefined
+//         subscribe(value => {
+//             value.forEach(vkp => {
+//                 if (vkp.address === address)
+//                     result = vkp.key
 
-            })
-        })
-        return result
-    }
+//             })
+//         })
+//         return result
+//     }
 
-    return { addViewingKey, getViewingKey }
-}
+//     return { addViewingKey, getViewingKey }
+// }
 
-const ViewingKeyStore = createStore();
-export default ViewingKeyStore
+// const ViewingKeyStore = createStore();
+// export default ViewingKeyStore
+
+export const viewingKey = persist<string>(writable(''), localStorage(), 'viewingKey')
