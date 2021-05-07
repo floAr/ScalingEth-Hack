950fdabe29eaa9abb5dc5d3e90fd33d1da8b3b3b
Florian Uhde <floar@mx.de>
2021-05-07 23:20:58 +0200

    temp work on collection

--- frontend/src/routes/collection.svelte
+++ frontend/src/routes/collection.svelte
@@ -1,6 +1,31 @@
-<script>
-import Mytokens from "$lib/components/mytokens.svelte";
+<script lang="ts">
+  import Mytokens from '$lib/components/mytokens.svelte'
+  import type { PublicToken } from '$lib/secret-manufaktur/token-store'
 
+  let predicate: (token: PublicToken) => boolean = undefined
+
+  const showAll = () => {
+    predicate = undefined
+  }
+  const showOnSale = () => {
+    predicate = token => {
+        console.log(token.name,token.price,token.price != null)
+      return token.price != null
+    }
+  }
 </script>
 
-<Mytokens />
+<div class="collection-header">
+  <button on:click={showAll}>All tokens</button>
+  <button on:click={showOnSale}>On Sale</button>
+  <button>All tokens</button>
+</div>
+<Mytokens {predicate} />
+
+<style lang="scss">
+  .collection-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+</style>
