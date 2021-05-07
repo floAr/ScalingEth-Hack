3ed19a0b34da8e1ffa3014a677b67d298c993c72
Florian Uhde <floar@mx.de>
2021-05-07 21:53:46 +0200

    using viewing key again

--- frontend/src/lib/secret-manufaktur/token-provider.svelte
+++ frontend/src/lib/secret-manufaktur/token-provider.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { browser } from '$app/env'
   import { SecretStore, selectedAccount } from '$lib/modules/secret/secret-store'
-  // import     from '$lib/modules/secret/viewingkey-store'
+  import viewingKey from '$lib/modules/secret/viewingkey-store'
+
   import { tokenContract } from './contract-interaction'
 
   import { AllTokensStore, MyTokensStore } from './token-store'
@@ -22,15 +23,15 @@
   }
 
   async function loadMyTokens() {
-    // let vkey = ViewingKeyStore.getViewingKey($selectedAccount.address)
-    // if (vkey === undefined) {
-    //   vkey = await ViewingKeyStore.addViewingKey($selectedAccount.address)
-    // }
+    let vkey = viewingKey.getViewingKey($selectedAccount.address)
+    if (vkey === undefined) {
+      vkey = await viewingKey.addViewingKey($selectedAccount.address)
+    }
     const mytokens = await tokenContract.SendQuery({
       tokens: { owner: $selectedAccount.address, viewing_key: '' }
     })
     MyTokensStore.set(mytokens.token_list.tokens)
-    console.log("my tokens:",$MyTokensStore)
+    console.log('my tokens:', $MyTokensStore)
   }
 
   if (browser) {
@@ -50,4 +51,3 @@
       }
   }
 </script>
-
