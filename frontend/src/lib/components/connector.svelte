b484aac339f06b980db2bb053a228f672af2f340
Florian Uhde <floar@mx.de>
2021-05-08 08:49:43 +0200

    components for catching authorized routes

---
+++ frontend/src/lib/components/connector.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { browser } from '$app/env'
+import { goto } from '$app/navigation';
+  import { currentRedirect } from '$lib/modules/secret/redirect-store'
+  import { SecretStore } from '$lib/modules/secret/secret-store'
+  import { onMount } from 'svelte'
+
+  export let redirect = '.'
+  onMount(() => {
+    if (browser) {
+      if (!$SecretStore.connected) {
+        $currentRedirect = redirect
+        goto('/connect')
+      }
+    }
+  })
+</script>
---
+++ frontend/src/routes/connect.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { browser } from '$app/env'
+
+  import { goto } from '$app/navigation'
+  import { currentRedirect } from '$lib/modules/secret/redirect-store'
+
+  import { SecretStore } from '$lib/modules/secret/secret-store'
+
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    if (browser) {
+      if ($SecretStore.connected) goto($currentRedirect)
+    }
+  })
+
+  $: {
+    if (browser) {
+      if ($SecretStore.connected) goto($currentRedirect)
+    }
+  }
+</script>
+
+<div>Please connect to keplr</div>
