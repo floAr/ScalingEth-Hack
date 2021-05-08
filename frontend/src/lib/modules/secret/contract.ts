2d7ca6c79e18957f11e078571a560b6f94f1d11f
Florian Uhde <floar@mx.de>
2021-05-08 16:45:48 +0200

    buttons now have logic <3

--- frontend/src/lib/modules/secret/contract.ts
+++ frontend/src/lib/modules/secret/contract.ts
@@ -1,3 +1,4 @@
+import type { Coin } from "secretjs/types/types";
 import { query, transact } from "./contract-helper";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -8,12 +9,12 @@ class Contract<Init, QueryMsg extends object, QueryAnswer, HandleMsg extends obj
         this.address = address;
     }
 
-    SendQuery(queryObj: QueryMsg):Promise<QueryAnswer> {
+    SendQuery(queryObj: QueryMsg): Promise<QueryAnswer> {
         return query<QueryMsg, QueryAnswer>(this.address, queryObj)
     }
 
-    SendTransaction(transactionObj: HandleMsg):Promise<HandleAnswer> {
-        return transact<HandleMsg, HandleAnswer>(this.address, transactionObj)
+    SendTransaction(transactionObj: HandleMsg, fee?: Coin): Promise<HandleAnswer> {
+        return transact<HandleMsg, HandleAnswer>(this.address, transactionObj, fee)
     }
 }
 
--- frontend/src/lib/secret-manufaktur/buttonSets.ts
+++ frontend/src/lib/secret-manufaktur/buttonSets.ts
@@ -1,13 +1,44 @@
+import { tokenContract } from "./contract-interaction";
 import { isMyToken, PublicToken } from "./token-store";
 import type { FlowButton } from "./types";
 
+const getPrice = () => {
+    const scrt_price = prompt("Enter a price in SCRT")
+    return Number(scrt_price) * 1000000
+}
+
 export const buttons: FlowButton[] = [
     // buy only if it has a price and is not owned by myself
-    { title: 'buy', func: (t) => { console.log("buy") }, active: (t:PublicToken):boolean => { return t.price != null && !isMyToken(t.id)} },
+    {
+        title: 'buy', func: (t) => {
+            tokenContract.SendTransaction({ buy: { token_id: t.id } }, { denom: 'uscrt', amount: t.price })
+        }, active: (t: PublicToken): boolean => { return t.price != null && !isMyToken(t.id) }
+    },
     // list only if it is my token and has no price yet
-    { title: 'list', func: (t) => { console.log("buy") }, active: (t) => { return t.price == null && isMyToken(t.id) } },
+    {
+        title: 'list', func: (t) => {
+            const price = getPrice()
+            if (price >= 0) {
+                tokenContract.SendTransaction({ set_price: { token_id: t.id, price: price.toString() } })
+            }
+        }, active: (t) => { return t.price == null && isMyToken(t.id) }
+    },
     // unlist only if it is my token and has a price set
-    { title: 'unlist', func: (t) => { console.log("buy") }, active: (t) => { return t.price != null && isMyToken(t.id) } },
-     // change price only if it is my token and has a price set
-     { title: 'change price', func: (t) => { console.log("buy") }, active: (t) => { return t.price != null && isMyToken(t.id) } },
+    {
+        title: 'unlist', func: (t) => {
+            const price = getPrice()
+            if (price >= 0) {
+                tokenContract.SendTransaction({ unlist: { token_id: t.id } })
+            }
+        }, active: (t) => { return t.price != null && isMyToken(t.id) }
+    },
+    // change price only if it is my token and has a price set
+    {
+        title: 'change price', func: (t) => {
+            const price = getPrice()
+            if (price >= 0) {
+                tokenContract.SendTransaction({ set_price: { token_id: t.id, price: price.toString() } })
+            }
+        }, active: (t) => { return t.price != null && isMyToken(t.id) }
+    },
 ]
